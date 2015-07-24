40cb021095dc4eaa972339ecd57c5e1b08541ad4
Coleman Cox <colezane82@gmail.com>
2015-07-23 21:24:37 -0500

    trying to get this to link the right css

--- Brocfile.js
+++ Brocfile.js
@@ -3,13 +3,13 @@ var sassCompile = require('broccoli-sass');
 
 var includePaths = [
   'assets',
-  bower_components/rest-css,
-  bower_components/fontawesome/scss,
-  bower_components/bourbon/app/assets/stylesheets,
-  bower_components/neat/app/assets/stylesheets
+ 'bower_components/reset-css',
+  'bower_components/fontawesome/scss',
+  'bower_components/bourbon/app/assets/stylesheets',
+  'bower_components/neat/app/assets/stylesheets'
 
 ];
 
-var compiledCss = sassCompile(includePaths,'app.scss', 'app.css');
+var compiledCss = sassCompile(includePaths,'app.scss', 'accordian.css');
 module.exports = merge(['public', compiledCss]);
 
