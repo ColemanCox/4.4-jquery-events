dd132f8db83f3fedebfac9238d598208f2b730fb
Coleman Cox <colezane82@gmail.com>
2015-07-26 23:32:49 -0500

    not sure if the values of my buttons are actually called 'values' or not

--- public/keyboard.js
+++ public/keyboard.js
@@ -1,7 +1,8 @@
  //not sure how to logbuttons exactly
   // I know it needs to be something like button + last button or something
+var buttonEl = document.getElementsByType('button');
+var deleteEl = document.getElementsByClass('delete')
+deleteEl.onclick = function() {
 
-
-function li() {
-  li.onclick = alert("hey");
 }
+
