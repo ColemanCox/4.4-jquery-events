9d0b4754b9b3f08ff3c67725e7012f9f04aeaad6
Coleman Cox <colezane82@gmail.com>
2015-07-26 20:19:15 -0500

    got sum features goin on accordian. Still iffy on js'n a shifty menu

--- public/keyboard.js
+++ public/keyboard.js
@@ -1,4 +1,7 @@
-function Button() {
-  this.onclick = ' '; //not sure how to logbuttons exactly
+ //not sure how to logbuttons exactly
   // I know it needs to be something like button + last button or something
+
+
+function li() {
+  li.onclick = alert("hey");
 }
