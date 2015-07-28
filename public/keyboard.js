6a1e05090924a63261dda60c9ff9cee81a5f60a7
Coleman Cox <colezane82@gmail.com>
2015-07-27 23:45:10 -0500

    todays new methods add to my confusion

--- public/keyboard.js
+++ public/keyboard.js
@@ -1,8 +1,13 @@
  //not sure how to logbuttons exactly
-  // I know it needs to be something like button + last button or something
+ // I know it needs to be something like button + last button or something
+var valueEl = document.getElementById('value');
 var buttonEl = document.getElementsByType('button');
 var deleteEl = document.getElementsByClass('delete');
+
 deleteEl.onclick = function() {
+  valueEl.innerHTML = button.value + previous + previous;
+
+
 //I want to first get the button values to display
 //then i want them to string together when you type
 //next I wanna be able to delete unwanted letters
